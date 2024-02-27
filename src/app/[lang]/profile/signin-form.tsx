5541ead222b1c91286bee9f545e72f88f19c2cc9
barham <baranali6688@gmail.com>
2024-02-27 07:08:38 +0300

    Sign-in form changes

--- src/app/[lang]/profile/signin-form.tsx
+++ src/app/[lang]/profile/signin-form.tsx
@@ -3,6 +3,7 @@ import { Login, getProfile } from "@/actions";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import FormButton from "@/components/ui/form-button";
 import { Input } from "@/components/ui/input";
+import Link from "next/link";
 import { useFormState } from "react-dom";
 import { twMerge } from "tailwind-merge";
 
@@ -14,9 +15,10 @@ export default function SigninForm({ lang }: { lang: "kur" | "eng" }) {
       <h1 className="text-2xl">
         {isKrd ? "زانیاریەکانت وەرگرە" : "Get your profile"}
       </h1>
+
       <form
         action={FormAction}
-        className={twMerge("flex w-1/2 flex-col items-center gap-3")}
+        className={twMerge("flex w-[90%] flex-col items-center gap-3 lg:w-1/2")}
       >
         <div
           className={twMerge(
@@ -34,6 +36,16 @@ export default function SigninForm({ lang }: { lang: "kur" | "eng" }) {
           />
           <input type="text" name="lang" defaultValue={lang} hidden />
           <FormButton>{isKrd ? "بچۆ ژوورەوە" : "Sign In"}</FormButton>
+          <p className={twMerge("text-left", isKrd && "text-right")}>
+            <Link
+              href={isKrd ? "/kur/register" : "/eng/register"}
+              className=" text-[#042c61]"
+            >
+              {isKrd
+                ? "کلیك لێرە بکە بۆ بەشداری کردن"
+                : "Click here to Sign Up"}
+            </Link>
+          </p>
         </div>
         {state.message && (
           <Alert variant="destructive" className=" flex justify-center">
