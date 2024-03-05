fed5f97aad774a667b30b45520a03e77e136a774
baran6688 <baranali6688@gmail.com>
2024-03-05 20:58:31 +0300

    Sitemap change

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,12 @@
 export default function sitemap() {
+  const base = "https://www.smartvolunteersgroup.com";
   const krdUrl = "https://www.smartvolunteersgroup.com/kur";
   const engUrl = "https://www.smartvolunteersgroup.com/eng";
   return [
+    {
+      url: base,
+      lastModified: new Date(),
+    },
     {
       url: krdUrl,
       lastModified: new Date(),
