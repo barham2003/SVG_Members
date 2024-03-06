a28395a5386531eff973c2596f68608a002f1bb6
barham <baranali6688@gmail.com>
2024-03-06 21:00:00 +0300

    Some MetaData Changes

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -1,5 +1,21 @@
 import Navbar from "@/components/navbar";
+import { Metadata } from "next";
 import NextTopLoader from "nextjs-toploader";
+
+export const metadata: Metadata = {
+  openGraph: {
+    images: { url: "https://i.ibb.co/7VRM64d/image-2024-03-05-122753434.png" },
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Smart Volunteers Group",
+    description: "Smart Volunteers Group",
+    siteId: "1467726470533754880",
+    creator: "@nextjs",
+    creatorId: "1467726470533754880",
+    images: ["https://i.ibb.co/7VRM64d/image-2024-03-05-122753434.png"], // Must be an absolute URL
+  },
+};
 
 export default function layout({
   children,
