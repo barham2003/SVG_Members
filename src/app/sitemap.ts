f8653d6a45544cf61a5837d13609cf60e8a3453a
baran6688 <baranali6688@gmail.com>
2024-03-22 17:57:30 +0300

    Sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,26 +1,12 @@
 export default function sitemap() {
   const base = "https://www.smartvolunteersgroup.com";
-  const krdUrl = "https://www.smartvolunteersgroup.com/ku";
-  const enUrl = "https://www.smartvolunteersgroup.com/en";
   return [
     {
       url: base,
       lastModified: new Date(),
     },
     {
-      url: krdUrl,
-      lastModified: new Date(),
-    },
-    {
-      url: enUrl,
-      lastModified: new Date(),
-    },
-    {
-      url: `${enUrl}/activities`,
-      lastModified: new Date(),
-    },
-    {
-      url: `${krdUrl}/activities`,
+      url: `/activities`,
       lastModified: new Date(),
     },
   ];
